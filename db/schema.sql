3d687dfe03bc2ae2bdff112068efbbffa29112bd
Neta2393 <benetheahardin@yahoo.com>
2023-06-25 21:38:19 -0700

    Added roles and employee names to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,46 @@
+-- The line of code below is creating the database and sourcing the contents of the table within the database
 DROP DATABASE IF EXISTS tasklist_db;
 CREATE DATABASE tasklist_db;
-
+-- The lines of code below are telling mysql to use the tasklist_db
 USE tasklist_db;
+-- The line of code below is telling mysql the information that I would like to include in my employees table. Its creating the table titles and adding the different 
 
-CREATE TABLE employees (
-  id INT NOT NULL,
-  job_title VARCHAR(30) NOT NULL,
-  job_description TEXT NOT NULL,
-  active BOOLEAN NOT NULL,
+CREATE TABLE department (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+name VARCHAR(30) NOT NULL,
+
+PRIMARY KEY(id)
+
+);
+
+CREATE TABLE role (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+title VARCHAR(30) NOT NULL,
+
+salary DECIMAL(10,2) NOT NULL,
+
+department_id INT NOT NULL,
+
+PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+first_name VARCHAR(30) NOT NULL,
+
+last_name VARCHAR(30) NOT NULL,
+
+role_id INT NOT NULL,
+
+manager_id INT,
+
+PRIMARY KEY (id)
+
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,12 @@
-INSERT INTO employees (first_name, last_name, assignment)
-VALUES ("Benethea", "Hardin", true),
-       ("Crystal", "Schwab", true),
-       ("James", "Brown", true),
-       ("Koa", "Boittnot", true),
-       ("Sam", "Smith", true),
-       ("Michael", "Jackson", true);
+-- The lines of code below are adding the order of the employees. I would like to generate the department, role, name and assignment
+INSERT INTO department (name)
+VALUES ("CE"), ("ECE"), ("TDS"), ("HR"), ("Management/Admin");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("DSP I", 40000.00, 2), ("DSP II", 43000.00, 3), ("DSP III", 50000.00, 4), ("Human Resources", 60000.00, 1), ("Area Manager", 85000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Benethea", "Hardin", 1, 3), ("Michael", "Jackson", 3, 2), ("Sam", "Smith", 4, 4), ("James", "Brown", 5, 2), ("Pepe", "Le Pew", 5, 2);
+
 
        
